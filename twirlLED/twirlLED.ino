530ce4fbf6ea080d3e83f571257f08a8cbf2b2fb
Kyle Hershey <kwhershey@gmail.com>
2017-10-22 19:21:30 -0500

    tuning to installed system

--- twirlLED/twirlLED.ino
+++ twirlLED/twirlLED.ino
@@ -4,21 +4,27 @@
 #define LED_PIN    26       // change to your data pin
 #define BUTTON_PIN 30 
 #define COLOR_ORDER GRB      // if colors are mismatched; change this
-#define NUM_LEDS    900       // change to the number of LEDs in your strip
+#define NUM_LEDS    598       // change to the number of LEDs in your strip
 #define BRIGHTNESS 32
 #define WRAP_NUM 46 
 #define LED_TYPE    WS2812B
 
 CRGB leds[NUM_LEDS];
 int startIndex=0;
-int bottom=5;
+int bottom=2;
 int buttonState=0;
 int lastButtonState=0;
 int pushCounter=0;
-int numPrograms=8;
+int numPrograms=9;
+
+// pong variables
+int c_x=0;
+int c_y=0;
+int c_dx=1;
+int c_dy=1;
 
 // matrix variables
-#define numParticles 15
+#define numParticles 30
 int lastRow[numParticles];
 int column[numParticles]; 
 
@@ -64,6 +70,9 @@ void loop()
     else if(pushCounter%numPrograms==6){
         manTwirl();
     }
+    else if(pushCounter%numPrograms==7){
+        pong();
+    }
     else{
         SITwirl();
     }
@@ -81,36 +90,11 @@ void rainbowLineTwirl()
 {
     FastLED.clear();
     for(int i=0;i<NUM_LEDS;i++){
-        if(i%WRAP_NUM==startIndex){
+      for(int j=0;j<10;j++){
+        if(i%WRAP_NUM==(startIndex+j*WRAP_NUM/10)%WRAP_NUM){
             leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
         }
-        else if(i%WRAP_NUM==(startIndex+1*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+2*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+3*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+4*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+5*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+6*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+7*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+8*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
-        else if(i%WRAP_NUM==(startIndex+9*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setHSV(startIndex*255/WRAP_NUM,255,255);
-        }
+      }
     }
     FastLED.show();
     delay(30);
@@ -123,52 +107,32 @@ void lineTwirl()
 {
     FastLED.clear();
     for(int i=0;i<NUM_LEDS;i++){
-        if(i%WRAP_NUM==startIndex){
+      for(int j=0;j<10;j++){
+        if(i%WRAP_NUM==(startIndex+j*WRAP_NUM/10)%WRAP_NUM){
             leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
         }
-        else if(i%WRAP_NUM==(startIndex+1*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+2*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+3*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+4*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+5*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+6*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+7*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+8*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
-        else if(i%WRAP_NUM==(startIndex+9*WRAP_NUM/10)%WRAP_NUM){
-            leds[i].setRGB(0,abs(255-510*(float(startIndex)/WRAP_NUM)),255-abs(255-510*(float(startIndex)/WRAP_NUM)));
-        }
+      }
     }
     FastLED.show();
     delay(30);
 
     startIndex=(startIndex+1)%WRAP_NUM;
+    
 }
 
 //heart
 void heartTwirl()
 {
     FastLED.clear();
-    static int heart[]={bottom*WRAP_NUM,(bottom+1)*WRAP_NUM-1,(bottom+1)*WRAP_NUM,(bottom+1)*WRAP_NUM+1,(bottom+2)*WRAP_NUM,(bottom+2)*WRAP_NUM-1,(bottom+2)*WRAP_NUM-2,(bottom+2)*WRAP_NUM+1,(bottom+2)*WRAP_NUM+2,(bottom+3)*WRAP_NUM-1,(bottom+3)*WRAP_NUM+1};
+    static int heart_x[]={0,-1,0,1,0,-1,-2,1,2,-1,1};
+    static int heart_y[]={0,1,1,1,2,2,2,2,2,3,3};
     int x = 0;
-    for(int i=0;i<(sizeof(heart)/sizeof(int));i++)
+    for(int i=0;i<(sizeof(heart_x)/sizeof(int));i++)
     {
-        leds[int(heart[i]+startIndex)]=CRGB::Red;
+        leds[int((heart_x[i]+startIndex+WRAP_NUM/4))%WRAP_NUM+(bottom+heart_y[i])*WRAP_NUM]=CRGB::Red;
+        leds[int((heart_x[i]+startIndex+WRAP_NUM/2))%WRAP_NUM+(bottom+heart_y[i])*WRAP_NUM]=CRGB::Blue;
+        leds[int((heart_x[i]+startIndex+WRAP_NUM*3/4))%WRAP_NUM+(bottom+heart_y[i])*WRAP_NUM]=CRGB::Green;
+        leds[int((heart_x[i]+startIndex))%WRAP_NUM+(bottom+heart_y[i])*WRAP_NUM]=CRGB::Yellow;
     }
     FastLED.show();
     delay(30);
@@ -180,10 +144,18 @@ void heartTwirl()
 void diamondTwirl()
 {
     FastLED.clear();
-    static int diamond[]={bottom*WRAP_NUM,(bottom+1)*WRAP_NUM-1,(bottom+1)*WRAP_NUM+1,(bottom+2)*WRAP_NUM};
+    static int diamond[]={WRAP_NUM,(1)*WRAP_NUM-1,(1)*WRAP_NUM+1,(2)*WRAP_NUM};
+    static int diamond_x[]={0,-1,1,0};
+    static int diamond_y[]={0,1,1,2};
     for(int i=0;i<(sizeof(diamond)/sizeof(int));i++)
-    {
-        leds[int(diamond[i]+startIndex)]=CRGB::Red;
+    {      
+      for(int j=0;j<9;j++)
+      {
+        for(int k=0;k<3;k++)
+        {   
+          leds[(diamond_x[i]+startIndex+j*WRAP_NUM/9)%WRAP_NUM+(diamond_y[i]+k*4+1)*WRAP_NUM]=CRGB::Red;
+        }
+      }
     }
     FastLED.show();
     delay(30);
@@ -195,15 +167,17 @@ void diamondTwirl()
 void manTwirl()
 {
     FastLED.clear();
-    static int man[]={bottom*WRAP_NUM,(bottom+1)*WRAP_NUM+1, (bottom+2) *WRAP_NUM+1, (bottom+2) *WRAP_NUM+4, (bottom+3) *WRAP_NUM+2,(bottom+3) *WRAP_NUM+3,(bottom+3) *WRAP_NUM+4,(bottom+4) *WRAP_NUM+2,(bottom+5) *WRAP_NUM,(bottom+5) *WRAP_NUM+2,(bottom+6) *WRAP_NUM,(bottom+6) *WRAP_NUM+1,(bottom+6) *WRAP_NUM+2,(bottom+6) *WRAP_NUM+3,(bottom+6) *WRAP_NUM+4,(bottom+6) *WRAP_NUM+5,(bottom+7) *WRAP_NUM+2,(bottom+7) *WRAP_NUM+5,(bottom+8) *WRAP_NUM+1,(bottom+8) *WRAP_NUM+2,(bottom+9) *WRAP_NUM+1,(bottom+9) *WRAP_NUM+2,(bottom+9) *WRAP_NUM+3,(bottom+10) *WRAP_NUM+1,(bottom+10) *WRAP_NUM+2,(bottom+10) *WRAP_NUM+3, };
-    static int eye[]={(bottom+8) *WRAP_NUM+3};
-    for(int i=0;i<(sizeof(man)/sizeof(int));i++)
+    static int man_x[]={0,1,1,4,2,3,4,2,0,2,0,1,2,3,4,5,2,5,1,2,1,2,3,1,2,3};
+    static int eye_x[]={3};
+    static int man_y[]={0,1,2,2,3,3,3,4,5,5,6,6,6,6,6,6,7,7,8,8,9,9,9,10,10,10};
+    static int eye_y[]={9};
+    for(int i=0;i<(sizeof(man_x)/sizeof(int));i++)
     {
-        leds[int(man[i]+startIndex)]=CRGB::Green;
+        leds[int((man_x[i]+startIndex))%WRAP_NUM+man_y[i]*WRAP_NUM]=CRGB::Green;
     }
-    for(int i=0;i<(sizeof(eye)/sizeof(int));i++)
+    for(int i=0;i<(sizeof(eye_x)/sizeof(int));i++)
     {
-        leds[int(eye[i]+startIndex)]=CRGB::Red;
+        leds[int((eye_x[i]+startIndex))%WRAP_NUM+eye_y[i]*WRAP_NUM]=CRGB::Red;
     }
     FastLED.show();
     delay(30);
@@ -217,12 +191,11 @@ void manTwirl()
 void SITwirl()
 {
     FastLED.clear();
-    static int spaceInvader[]={(bottom)*WRAP_NUM,(bottom)*WRAP_NUM+1,(bottom)*WRAP_NUM+3,(bottom)*WRAP_NUM+4,(bottom+1)*WRAP_NUM-3,(bottom+1)*WRAP_NUM-1,(bottom+1)*WRAP_NUM+5,(bottom+1)*WRAP_NUM+7,(bottom+2)*WRAP_NUM-3,(bottom+2)*WRAP_NUM-1,(bottom+2)*WRAP_NUM,(bottom+2)*WRAP_NUM+1,(bottom+2)*WRAP_NUM+2,(bottom+2)*WRAP_NUM+3,(bottom+2)*WRAP_NUM+4,(bottom+2)*WRAP_NUM+5,(bottom+2)*WRAP_NUM+7,
-    (bottom+3)*WRAP_NUM-3,(bottom+3)*WRAP_NUM-2,(bottom+3)*WRAP_NUM-1,(bottom+3)*WRAP_NUM,(bottom+3)*WRAP_NUM+1,(bottom+3)*WRAP_NUM+2,(bottom+3)*WRAP_NUM+3,(bottom+3)*WRAP_NUM+4,(bottom+3)*WRAP_NUM+5,(bottom+3)*WRAP_NUM+6,(bottom+3)*WRAP_NUM+7,(bottom+4)*WRAP_NUM-2,(bottom+4)*WRAP_NUM-1,(bottom+4)*WRAP_NUM+1,(bottom+4)*WRAP_NUM+2,(bottom+4)*WRAP_NUM+3,(bottom+4)*WRAP_NUM+5,(bottom+4)*WRAP_NUM+6,
-    (bottom+5)*WRAP_NUM-1,(bottom+5)*WRAP_NUM,(bottom+5)*WRAP_NUM+1,(bottom+5)*WRAP_NUM+2,(bottom+5)*WRAP_NUM+3,(bottom+5)*WRAP_NUM+4,(bottom+5)*WRAP_NUM+5,(bottom+6)*WRAP_NUM,(bottom+6)*WRAP_NUM+4,(bottom+7)*WRAP_NUM-1,(bottom+7)*WRAP_NUM+5};
-    for(int i=0;i<(sizeof(spaceInvader)/sizeof(int));i++)
+    static int spaceInvader_x[]={0,1,3,4,-3,-1,5,7,-3,-1,0,1,2,3,4,5,7,-3,-2,-1,0,1,2,3,4,5,6,7,-2,-1,1,2,3,5,6,-1,0,1,2,3,4,5,0,4,-1,5};
+    static int spaceInvader_y[]={0,0,0,0,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,5,5,5,5,5,5,5,6,6,7,7};
+    for(int i=0;i<(sizeof(spaceInvader_x)/sizeof(int));i++)
     {
-        leds[int(spaceInvader[i]+startIndex)]=CRGB::Red;
+        leds[int((spaceInvader_x[i]+startIndex))%WRAP_NUM+(spaceInvader_y[i]+bottom)*WRAP_NUM]=CRGB::Red;
     }
     FastLED.show();
     delay(30);
@@ -263,9 +236,9 @@ void checkerboard()
     FastLED.clear();
     for(int i=0;i<WRAP_NUM;i++){
         for(int j=0;j<(NUM_LEDS/WRAP_NUM)+1;j++){
-            if(j*WRAP_NUM+i<NUM_LEDS){
+            if(j*WRAP_NUM+(i+WRAP_NUM/2)%WRAP_NUM<NUM_LEDS){
                 if((i/4)%2==(startIndex+j/4)%2){
-                    leds[j*WRAP_NUM+i].setHSV(startIndex*255/WRAP_NUM,255,255);
+                    leds[j*WRAP_NUM+(i+WRAP_NUM/2)%WRAP_NUM].setHSV(startIndex*255/WRAP_NUM,255,255);
             }
             }
         }
@@ -274,4 +247,29 @@ void checkerboard()
     delay(600);
 
     startIndex=(startIndex+1)%WRAP_NUM;
+}
+
+
+// pong
+void pong()
+{
+    FastLED.clear();
+    leds[c_x%WRAP_NUM+c_y*WRAP_NUM]=CRGB::Red;
+    if(c_x==17){
+      c_dx=-1;
+    }
+    else if(c_x==-17){
+      c_dx=1;
+    }
+    if(c_y==12){
+      c_dy=-1;
+    }
+    else if(c_y==0){
+      c_dy=1;
+    }
+    c_x=c_x+c_dx;
+    c_y=c_y+c_dy;
+    FastLED.show();
+    delay(30);
+
 }
